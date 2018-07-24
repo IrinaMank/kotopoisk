a00a18e22e46a9596b322da7305d5f4d5771a676
IrinaMank <zapirinamank@gmail.com>
2018-07-24 12:32:55 +0700

    auth v 1.0

---
+++ app/src/main/java/com/example/zapir/kotopoisk/ProfileFragment.kt
@@ -0,0 +1,41 @@
+package com.example.zapir.kotopoisk
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.profile_fragment.*
+
+
+class ProfileFragment : Fragment() {
+
+    companion object {
+
+        fun newInstance(): ProfileFragment {
+            return ProfileFragment()
+        }
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sign_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(context, AuthActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/zapir/kotopoisk/ProfileHolderFragment.kt
@@ -0,0 +1,14 @@
+package com.example.zapir.kotopoisk
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.View
+
+class ProfileHolderFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
