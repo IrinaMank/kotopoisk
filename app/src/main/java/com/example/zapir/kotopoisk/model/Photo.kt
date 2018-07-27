91c447b0fcb03f4758a23beb4b64a435ee56b0ac
avoloshina <q>
2018-07-27 17:06:46 +0700

    activityOnResult don't work, i've no ideas why

--- app/src/main/java/com/example/zapir/kotopoisk/model/Photo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.zapir.kotopoisk.model
-
-import java.util.*
-
-data class Photo(val id: String = UUID.randomUUID().toString(),
-                 val url: String = "",
-                 val userId: String = "",
-                 val animalId: String = "")
